927163c8f1aeb60562f3fd06cd0e20320faf1fda
Bryan <bryan@devlift.io>
2023-03-07 15:51:11 -0500

    added all the types for employment records

---
+++ src/types/employment-record.model.ts
@@ -0,0 +1,9 @@
+export interface EmploymentRecordModel {
+    companyName: string;
+    dates: string;
+    employmentType: string[];
+    keyPoints: string[];
+    order: number;
+    technologiesIcon: string;
+    title: string;
+}
---
+++ src/types/index.d.ts
@@ -0,0 +1,2 @@
+export * from "./employment-record.model";
+export * from "./helpers.model";
