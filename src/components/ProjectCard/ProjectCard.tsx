6313c00cc318307a5496398ce69768795df1e2c6
Bryan <bryan@devlift.io>
2023-03-07 22:51:14 -0500

    added boilerplates for the projects section

---
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,14 @@
+// @ts-ignore: Cannot find css module
+import styles from './ProjectCard.module.css';
+
+interface ProjectCardProps {}
+
+const ProjectCard = ({ }: ProjectCardProps) => {
+  return (
+    <div>
+      <h2>ProjectCard</h2>
+    </div>
+  );
+}
+
+export default ProjectCard;
---
+++ src/components/ProjectsList/ProjectsList.tsx
@@ -0,0 +1,14 @@
+// @ts-ignore: Cannot find css module
+import styles from './ProjectsList.module.css';
+
+interface ProjectsListProps {}
+
+const ProjectsList = ({ }: ProjectsListProps) => {
+  return (
+    <div>
+      <h2>ProjectsList</h2>
+    </div>
+  );
+}
+
+export default ProjectsList;
