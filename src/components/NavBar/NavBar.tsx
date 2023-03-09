6c9db520f5f27654d5fbed4ebf3a1bf1541afc4a
Bryan <bryan@devlift.io>
2023-03-09 06:32:11 -0500

    added extra styling for smaller devices

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -23,23 +23,23 @@ const NavBar = ({ setDarkMode, showLogout }: NavBarProps) => {
 
   return (
     <nav className="pt-10 flex justify-between dark:text-white">
-      <h1 className="text-xl font-burtons">BMARAVILLADEV</h1>
+      <h1 className="text-sm font-burtons sm:text-xl mt-1">BMARAVILLADEV</h1>
       <ul className="flex items-center">
         <li>
           <BsFillMoonStarsFill
-            className="cursor-pointer text-2xl"
+            className="cursor-pointer text-md sm:text-xl md:text-2xl"
             onClick={() => setDarkMode(prevState => !prevState)}
           />
         </li>
         <li>
           <AiFillLinkedin
-            className="cursor-pointer ml-8 text-4xl"
+            className="cursor-pointer ml-2 text-2xl sm:ml-8 sm:text-4xl"
             onClick={handleLinkedIn}
           />
         </li>
         {showLogout && (
           <li
-            className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 cursor-pointer"
+            className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white text-sm px-2 py-1 rounded-md ml-4 cursor-pointer sm:px-4 sm:py-2 sm:ml-8 sm:text-base"
             onClick={handleLogout}
           >Logout</li>
         )}
